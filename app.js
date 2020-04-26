fbfd305b9b7af15e98b5fbffffb2bf3e04b7eba0
Ernest <emuroiwa@gmail.com>
2020-04-26 11:29:38 +0200

    enum

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('s');
+// Explict type casting
+//String Types 
+var myName = "Ernest";
+//number type
+var myAge = 33;
+//boolean type
+var myBool = true;
+//Arrays
+//tuples
+var myTuple = [12, 'fe'];
+//enum
+var Color;
+(function (Color) {
+    Color[Color["Black"] = 1] = "Black";
+    Color[Color["White"] = 3] = "White";
+    Color[Color["Orange"] = 2] = "Orange";
+})(Color || (Color = {}));
+var myColor = Color.Black;
+console.log(myColor);
--- app.ts
+++ app.ts
@@ -1,1 +1,23 @@
-console.log('s');
+// Explict type casting
+//String Types 
+let myName : string = "Ernest";
+
+//number type
+let myAge : number = 33;
+
+//boolean type
+let myBool : boolean = true;
+
+//Arrays
+//tuples
+let myTuple : [number, string] = [12,'fe'];
+
+//enum
+enum Color {
+    Black = 1,
+    White = 3,
+    Orange = 2,
+}
+ let myColor : Color = Color.Black;
+ console.log(myColor)
+
